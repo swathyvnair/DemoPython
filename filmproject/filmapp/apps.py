40b7e64d4ad8546c3eb1316425154fb6b1b837eb
swathyvnair <127477923+swathyvnair@users.noreply.github.com>
2023-03-15 18:52:06 +0530

    Delete filmproject directory

--- filmproject/filmapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FilmappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'filmapp'
--- filmproject/filmapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from.models import Film
-
-class Filmform(forms.ModelForm):
-    class Meta:
-        model=Film
-        fields=['name','desc','year','img']
